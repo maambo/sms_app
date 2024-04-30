2967069aa857b0912e27b9de45b549a694ef454f
maambo <maambomunkuli21@gmail.com>
2024-04-30 21:16:17 +0200

    home page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Center(
+      child: Column(children: []),
+    )));
+  }
+}
